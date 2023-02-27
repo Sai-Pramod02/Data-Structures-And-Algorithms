2b5e72a9f710154f83f3a631901a849d4d92d456
Sai-Pramod02 <107098747+Sai-Pramod02@users.noreply.github.com>
2023-02-27 21:25:07 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,9 @@
+list1 = [1,2,5,9,11] #Lists are ordered and can store data i.e numbers saperated by commas and enclosed in square brackets
+list2 = [2,"Hello",2.34,'h'] #A list can store any data type and also store multiple data types
+print(list1)
+print(list2)
+list1.append(3) #Lists are mutable 
+print(list1)
+del list1[0]
+print(list1)
+#Lists are dynamic
